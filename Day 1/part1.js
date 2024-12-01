4a787758718348349936d3113d9c6ec4b6c610e8
NoahR <95318960+MinecraftXPert@users.noreply.github.com>
2024-12-01 17:47:22 -0500

    Quickly change the input.txt file

--- Day 1/part1.js
+++ Day 1/part1.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const text = fs.readFileSync("input.txt").toString("utf-8");
+const text = fs.readFileSync("Day1/input1.txt").toString("utf-8");
 
 let arrayText = text.split("\r\n");
 
